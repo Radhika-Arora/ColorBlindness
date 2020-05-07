c80f6952006580b0ce2f00218be5f1b9b4ce9b0d
Radhika <57217604+Radhika-Arora@users.noreply.github.com>
2020-05-07 19:37:18 +0530

    Add files via upload

---
+++ CB.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Color Blindness</title>
+	<link href="https://fonts.googleapis.com/css2?family=Amiri:wght@700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="normalize.css">
+	<link rel="stylesheet" href="CB.css">
+</head>
+<body>
+	<section class="s1">
+		<div class="d1">
+			<h1>WHAT IS COLOR BLINDNESS</h1>
+			<p>Color-blindness is the inability to distinguish the differences between certain colors. This condition results from an absence of color-sensitive pigment in the cone cells of the retina, the nerve layer at the back of the eye.</p>
+			</div>
+	</section>
+
+	<section class="s2">
+		<div class="d2">
+			<img class="img-responsive" src="rg2.png" height="300px" width="300px" style="margin-left:450px;"  alt="">
+			<h2 class="heading" style="text-align:center;color:Red;">
+				Red-green color blindness
+			</h2>
+      <p> This is the most common form, resulting in problems distinguishing between some shades of red, yellow, and green. There are four subtypes within this spectrum, each of which causes different color deficiencies.</p>
+      <ul>
+        <li>Protanomaly: Red, orange, and yellow appear greener and colors are not as bright.</li>
+        <li>Protanopia: There are no working red cone cells, resulting in red appearing as black. Certain shades of orange, yellow, and green all look yellow.</li>
+        <li>Deuteranopia: There are no working green cone cells, so reds look brownish-yellow and greens appear beige.</li>
+        <li>Deuteranomaly is the most common type of red-green color blindness. It makes green look more red. This type is mild and doesn’t usually get in the way of normal activities.</li>
+      </ul>
+</div>
+
+		<div class="d2">
+			<img class="img-responsive" src="blueyellow.png" style="margin-left:450px;"  alt="">
+			<h2 class="heading"style="text-align:center;color:blue;">
+				Blue-yellow color blindness
+			</h2>
+			<p>This less-common type of color blindness makes it hard to tell the difference between blue and green, and between yellow and red.</p>
+      <ul>
+        <li>Tritanomaly makes it hard to tell the difference between blue and green, and between yellow and red.</li>
+        <li>Tritanopia makes you unable to tell the difference between blue and green, purple and red, and yellow and pink. It also makes colors look less bright.</li>
+		</div>
+		<div class="d2">
+			<img class="img-responsive" src="cc.png" style="margin-left:450px;" alt="">
+			<h2 class="heading"style="text-align:center;color:violet;">
+				Complete color blindness
+			</h2>
+			<p>If you have complete color blindness, you can’t see colors at all. This is also called monochromacy, and it’s quite uncommon. Depending on the type, you may also have trouble seeing clearly and you may be more sensitive to light.</p>
+		</div>
+
+	</section>
+  <div>
+  <section>
+    <h2 style="text-align:center ;color:tomato;">Treatment for color blindness</h2>
+    <p style="margin-left:100px;">There is no known cure for color blindness. Contact lenses and glasses are available with filters to help color deficiencies, if needed. Fortunately, the vision of most color-blind people is normal in all other respects and certain adaptation methods are all that is required. </p>
+    <img src="glass.png" style="margin-left:585px;"  height="300px" width="300px">
+  </div class="ab">
+</section>
+<br>
+<div class="right">
+<a  href="https://enchroma.com/pages/test"> Click here to know more</a>
+</div>
+</body>
+</html>
